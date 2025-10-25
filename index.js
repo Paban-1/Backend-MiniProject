b68a3e9cc5435ab5f109b7ec64cf239bad1e800c
Paban-1 <rpaban89@gmail.com>
2025-10-25 18:23:12 +0530

    Render: Render files name and content in a new route & a new file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { log } = require("console");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -14,11 +15,25 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, fileData) {
+      res.render('show', {filename: req.params.filename, fileData: fileData})
+    }
+  );
+});
+
 app.post("/create", function (req, res) {
   console.log(req.body);
-  fs.writeFile(`./files/${req.body.title.split(' ').join("")}.txt`,req.body.detiles, function (err){
-    res.redirect("/");
-  })
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.detiles,
+    function (err) {
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
