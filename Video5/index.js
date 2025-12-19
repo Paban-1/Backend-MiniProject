79f604c14db1c9e02e8b85adb61fc48077ebf4f9
Paban-1 <rpaban89@gmail.com>
2025-12-19 18:20:00 +0530

    dinamic routing & setup view engine

---
+++ Video5/index.js
@@ -0,0 +1,28 @@
+// Require express & path
+const express = require("express");
+const path = require("path");
+// initial expressas app
+const app = express();
+
+// Handing form with persers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+// Dinamic routing
+app.get("/profile/:userName", function (req, res) {
+  res.send(`Welcome, ${req.params.userName}`);
+});
+
+app.get("/author/:userName/:age", function (req, res) {
+  res.send(`Welcome, ${req.params.userName} of age: ${req.params.age}`);
+});
+
+app.listen(3000, function () {
+  console.log("Port is running at : 3000");
+});
---
+++ Video5/what learn.md
@@ -0,0 +1,23 @@
+##### Date -> 19-12-2025
+
+## initial a project with npm
+
+- Done
+
+## express install
+- Done
+
+## Dynamic routing
+- /auther/... change this one for every eatch user
+
+## How to get data coming from frontend at backend route
+
+## setting up persers for form
+
+- Done
+
+## setting up ejs for ejs pages.
+- npm i ejs
+- setup ejs as view engine
+
+## seting up public static files
